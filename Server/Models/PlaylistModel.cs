103ff11ced2704810f0bc7bedf3ca238ac9c052b
Dima Ballanar <D.Ballanar@gmail.com>
2023-06-28 19:43:01 +0300

    Server.csproj

--- Server/Models/PlaylistModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Server.Models;
-public class PlaylistModel
-{
-    public int Id { get; set; }
-    public string Username { get; set; }
-    public string Email { get; set; }
-    public string Password { get; set; }
-
-}
