b2db83e18d66b733c4f4cce7a3521db1a3b168ba
Dima Ballanar <D.Ballanar@gmail.com>
2023-07-02 21:10:36 +0300

    MiddleWare/MiddleWareExt.cs

---
+++ Server/TashMusicServer/MiddleWare/MiddleWareExt.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class Class1
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate m_Next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            m_Next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await m_Next(context);
+            }
+            catch (ArgumentNullException ex)
+            {
+                await HandleExceptionWithCodeAsync(context, HttpStatusCode.BadRequest, ex.Message);
+            }
+            catch (Exception ex)
+            {
+
+                await HandleExceptionWithCodeAsync(context, HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+
+        private Task HandleExceptionWithCodeAsync(HttpContext context, HttpStatusCode statusCode, string message)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)statusCode;
+
+            return context.Response.WriteAsync(message);
+        }
+    }
+}
+
