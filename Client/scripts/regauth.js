549bb824a9e045b9751f4145d9c0e13831784073
Dima Ballanar <D.Ballanar@gmail.com>
2023-07-12 18:45:30 +0300

    fix

--- Client/scripts/regauth.js
+++ Client/scripts/regauth.js
@@ -1,17 +1,23 @@
-let user = new Object();
+
 
 document.getElementById('registration').onclick = function () {
-    user.name = document.getElementById('username').value;
-    user.email = document.getElementById('email').value;
-    user.phonenumber = document.getElementById('phone').value;
-    user.password = document.getElementById('password').value;
+    const user = {
+        name: document.getElementById('username').value,
+        email: document.getElementById('email').value,
+        phonenumber: document.getElementById('phone').value,
+        password: document.getElementById('password').value,
+    };
+  registrationAPI(user);
+};
 
+function registrationAPI(user){
     let hxr = new XMLHttpRequest();
     hxr.open('POST', 'https://localhost:7172/api/user/');
     hxr.setRequestHeader('Content-Type', 'application/json');
     hxr.send(JSON.stringify(user));
-    hxr.onload=()=>console.log(hxr.response);
-};
+    hxr.onload = () => console.log(hxr.response);
+}
+//переписать на fetch
 
 
 
--- DatabaseScripts/20230625TashDb.sql
+++ DatabaseScripts/20230625TashDb.sql
@@ -1,7 +1,6 @@
 create database if not exists TashMusic;
 use TashMusic;
-insert into Users(name,email,number,password) value('admin', 'd.ballanar@gmail.com','+37525528579','admin12345');
-drop table users;
+
 create table if not exists Users (
  Id int primary key auto_increment,
     Name varchar(30) not null,
@@ -14,24 +13,34 @@ create table if not exists Users (
 create table if not exists Songs(
  Id int  primary key auto_increment,
     Name varchar(100) not null,
-    Name varchar(100) not null,
-    File_Path varchar(250) unique, 
-    Liked boolean default '0',
-    Genre_Id int,
-    foreign key(Genre_Id) references Genre_of_Music(id)
+    FilePath varchar(250) unique, 
+    GenreId int,
+    foreign key(Genre_Id) references GenreofMusic(id)
     );
 
-create table if not exists Genre_of_Music(
+create table if not exists GenreofMusic(
  Id int primary key auto_increment,
-    Genre_Type varchar(50) not null   
+    `Type` varchar(50) not null   
     );
 
 create table if not exists PlayList(
- Id int primary key auto_increment,    
- User_id int,
- foreign key(User_id) references Users(Id)
-    
+ Id int primary key auto_increment,   
+ name varchar(100) not null,
+ Userid int,
+ state int default 0, 
+ foreign key(User_id) references Users(Id)    
     );
     
-    drop  database tashmusic;
+    create table if not exists PlayListSongs(
+    id int primary key auto_increment,
+    Songid int not null,
+    Playlistid int not null,
+    LikeS boolean default 0,
+    foreign key(Songid) references Songs(Id),
+    foreign key(Playlistid) references Playlist(Id)
+    );
+    
+    
+    
+
 
