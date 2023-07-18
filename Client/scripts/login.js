a64136ab498fc1890c73aa67c0d647ff3be0302d
Dima Ballanar <D.Ballanar@gmail.com>
2023-07-18 16:59:14 +0300

    some version

--- Client/scripts/login.js
+++ Client/scripts/login.js
@@ -1,11 +1,20 @@
-let login=new Object();
-document.getElementById('logining').onclick=function(){
- login.NickName=document.getElementById('emailForLogin').value;
- login.Password=document.getElementById('passForLogin').value;
+let login = new Object();
+document.getElementById('logining').onclick = function () {
+    login.NickName = document.getElementById('emailForLogin').value;
+    login.Password = document.getElementById('passForLogin').value;
 
- let hxr=new XMLHttpRequest();
- hxr.open('POST','https://localhost:7172/api/Account/Login');
- hxr.setRequestHeader('Content-Type', 'application/json');
+    let hxr = new XMLHttpRequest();
+    hxr.open('POST', 'https://localhost:7172/api/Account/Login');
+    hxr.setRequestHeader('Content-Type', 'application/json');
     hxr.send(JSON.stringify(login));
-    hxr.onload=()=>console.log(hxr.response);
+    // hxr.onload=()=>console.log(hxr.response);
+    hxr.onload = () => {
+        const user = JSON.parse(hxr.response);       
+        if (user.accessToken != null) {
+            window.location.href = 'D:/TestJS/TashMusic/Client/index.html';           
+        }
+        else{
+            window.location.href='D:/TestJS/TashMusic/Client/login.html'
+        }
+    };   
 }
--- Server/Models/Repository/Product.cs
+++ Server/Models/Repository/Product.cs
@@ -4,8 +4,8 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }
-        public decimal Price { get; set; }
+        public string Path { get; set; }
+        
         public string Brand { get; set; }
         public string Category { get; set; }
 
