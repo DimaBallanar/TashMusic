677e12ca99291776f5aa2a450db74bbbb3c03cc8
Dima Ballanar <D.Ballanar@gmail.com>
2023-07-18 17:02:38 +0300

    music

--- Server/Models/Repository/Product.cs
+++ Server/Models/Repository/Product.cs
@@ -4,10 +4,8 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Path { get; set; }
-        
-        public string Brand { get; set; }
-        public string Category { get; set; }
+        public string FilePath { get; set; }
+        public int GenreId { get; set; }                      
 
     }
 }
