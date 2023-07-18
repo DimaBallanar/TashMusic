5039a84dd85f67c24461d9ef0d98a5e6f9b0016c
Dima Ballanar <D.Ballanar@gmail.com>
2023-07-18 17:48:09 +0300

    fix

---
+++ Server/Controllers/GenreController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MusicServer.Models.Repository;
+using MusicServer.Repository;
+using System.Data;
+
+namespace MusicServer.Controllers
+{
+    [ApiController]
+    //[Authorize(Roles = "Admin")]
+    [Route("api/[controller]")]
+    public class GenreController : ControllerBase
+    {
+        private readonly GenreRepository m_genreRepository;
+
+        public GenreController(GenreRepository genreRepository)
+        {
+            m_genreRepository = genreRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllGenre()
+        {
+            try
+            {
+                return Ok(m_genreRepository.GetAll());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Post(Genre genre)
+        {
+            try
+            {
+                return Ok(m_genreRepository.Put(genre));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }       
+    }
+}
